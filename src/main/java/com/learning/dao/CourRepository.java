810917597afbe6b71a028425fca809139b6f6e88
Hamza-1998 <talssi.hamza98@gmail.com>
2020-03-21 21:48:45 +0100

    CourService

--- src/main/java/com/learning/dao/CourRepository.java
+++ src/main/java/com/learning/dao/CourRepository.java
@@ -1,8 +1,14 @@
 package com.learning.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.learning.model.Cour;
 
 public interface CourRepository extends JpaRepository<Cour,Long > {
+	
+	@Query("SELECT c FROM Cour c WHERE (LOWER(c.name) LIKE CONCAT(?1, '%')) and c.module.id = ?2")
+	Page<Cour> findByNameAndModule(String name,Long idModule,Pageable pageable);
 }
--- src/main/java/com/learning/service/impl/CourServiceImpl.java
+++ src/main/java/com/learning/service/impl/CourServiceImpl.java
@@ -1,76 +1,127 @@
 package com.learning.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import com.learning.dao.CourRepository;
 import com.learning.dto.CourDTO;
 import com.learning.model.Cour;
+import com.learning.model.Module;
 import com.learning.model.base.Demande;
 import com.learning.model.base.PartialList;
 import com.learning.service.CourService;
+import com.learning.service.ModuleService;
 
 @Service
 public class CourServiceImpl implements CourService {
 
+	@Autowired
+	private CourRepository courRepository;
+	@Autowired
+	private ModuleService moduleService;
+	
+
+	// save or update
 	@Override
 	public CourDTO save(CourDTO courDTO) {
-		// TODO Auto-generated method stub
-		return null;
+		Cour cour = convertDTOtoModel(courDTO);
+		cour = courRepository.save(cour);
+		return convertModelToDTO(cour);
 	}
 
 	@Override
 	public CourDTO findById(long idOut) {
-		// TODO Auto-generated method stub
+		Optional<Cour> optional = courRepository.findById(idOut);
+
+		if (optional.isPresent()) {
+			Cour courFromDb = optional.get();
+			return convertModelToDTO(courFromDb);
+		}
 		return null;
 	}
 
 	@Override
-	public void delete(Cour t) {
-		// TODO Auto-generated method stub
-		
+	public void delete(Cour cour) {
+		courRepository.delete(cour);
 	}
 
 	@Override
 	public PartialList<CourDTO> findByCriteres(Demande<CourDTO> demande) {
-		// TODO Auto-generated method stub
-		return null;
+
+		CourDTO cour = demande.getModel();
+		int page = demande.getPage();
+		int size = demande.getSize();
+		Page<Cour> pageCour = courRepository.findByNameAndModule(cour.getName(),cour.getModule().getId(), PageRequest.of(page, size));
+
+		List<CourDTO> list = convertEntitiesToDtos(pageCour.getContent());
+		int totalElement = pageCour.getNumberOfElements();
+
+		return new PartialList<CourDTO>(totalElement, list);
 	}
 
 	@Override
-	public Cour convertDTOtoModel(CourDTO u) {
-		// TODO Auto-generated method stub
-		return null;
+	public Cour convertDTOtoModel(CourDTO courDTO) {
+		Cour cour = new Cour();
+		cour.setId(courDTO.getId());
+		cour.setName(courDTO.getName());
+
+		if (courDTO.getModule() != null) {
+			cour.setModule(moduleService.convertDTOtoModel(courDTO.getModule()));
+		}
+		return cour;
 	}
 
 	@Override
-	public CourDTO convertModelToDTO(Cour t) {
-		// TODO Auto-generated method stub
-		return null;
+	public CourDTO convertModelToDTO(Cour cour) {
+		CourDTO courDTO = new CourDTO();
+		courDTO.setId(cour.getId());
+		courDTO.setName(cour.getName());
+		Module module = cour.getModule();
+		if (module != null) {
+			courDTO.setModule(moduleService.convertModelToDTO(cour.getModule()));
+			
+		}
+		
+		courDTO.setCreatedAt(cour.getCreatedAt());
+		courDTO.setUpdatedAt(cour.getUpdatedAt());
+		return courDTO;
 	}
 
 	@Override
 	public PartialList<CourDTO> convertToListDTO(PartialList<Cour> list) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	@Override
-	public List<CourDTO> convertEntitiesToDtos(List<Cour> list) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Cour> convertDtosToEntities(List<CourDTO> list) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void deleteById(Long id) {
-		// TODO Auto-generated method stub
-		
+		courRepository.deleteById(id);
+
+	}
+
+	@Override
+	public List<CourDTO> convertEntitiesToDtos(List<Cour> cours) {
+		// basic methode
+		List<CourDTO> list = new ArrayList<CourDTO>();
+		for (Cour cour : cours) {
+			list.add(convertModelToDTO(cour));
+		}
+		return list;
+	}
+
+	@Override
+	public List<Cour> convertDtosToEntities(List<CourDTO> coursDTO) {
+		List<Cour> list = new ArrayList<Cour>();
+		for (CourDTO courDTO : coursDTO) {
+			list.add(convertDTOtoModel(courDTO));
+		}
+		return list;
 	}
 
 }
